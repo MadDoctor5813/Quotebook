676f392a3b68f248fd52653e1978ceb7dbe5de9d
Kevin <dragn194@gmail.com>
2017-03-08 23:26:30 -0500

    Make the quote page display a random quote

--- Quotebook/app/views.py
+++ Quotebook/app/views.py
@@ -5,7 +5,10 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
+from app.models import Quote
 from datetime import datetime
+import random
 
 def view_quote(request):
-	return render(request, "app/quote.html")
+    random_quote = random.choice(Quote.objects.all())
+    return render(request, "app/quote.html", {'quote': random_quote.quote, 'attribution': random_quote.attribution})
