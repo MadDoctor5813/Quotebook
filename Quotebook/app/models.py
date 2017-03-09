546e041d49277c942479c1d06204fd6647f0227e
Kevin <dragn194@gmail.com>
2017-03-08 23:12:33 -0500

    Add a quote model

--- Quotebook/app/models.py
+++ Quotebook/app/models.py
@@ -2,6 +2,15 @@
 Definition of models.
 """
 
+from django.contrib import admin
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+
+    quote = models.CharField(max_length=128)
+    attribution = models.CharField(max_length=64)
+
+    def __str__(self):
+        return 'Quote ' + str(self.pk) + ' by ' + self.attribution
+
+admin.site.register(Quote)
