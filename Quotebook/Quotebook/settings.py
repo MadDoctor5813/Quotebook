2701b2e732fd08ab4e25be1098d30f825cc9e629
Kevin <dragn194@gmail.com>
2017-03-09 15:14:09 -0500

    Load Postgres config from an environment variable

--- Quotebook/Quotebook/settings.py
+++ Quotebook/Quotebook/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import posixpath
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -83,6 +85,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#load database from URL
+db_from_url = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_url)
 
 
 # Password validation
