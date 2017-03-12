d876239710f7bf5d2ea392c95715242556cc02ec
Kevin <dragn194@gmail.com>
2017-03-11 20:10:43 -0500

    Improve pagination in all_quotes
    The paginator now returns page 1 if the page param is empty.
    
    In addition, we no longer submit an identical page_obj into the
    template. The bootstrap paginator now uses the same quotes object.

--- Quotebook/app/views.py
+++ Quotebook/app/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from django.core.paginator import Paginator
+from django.core.paginator import PageNotAnInteger
+from django.utils.datastructures import MultiValueDictKeyError
 from app.models import Quote
 from datetime import datetime
 import random
@@ -16,5 +18,11 @@ def view_quote(request):
 
 def view_all_quotes(request):
     all_quotes = Quote.objects.all()
-    paginator = Paginator(all_quotes, 5)
-    return render(request, "app/all_quotes.html", {'quotes' : paginator.page(request.GET['page']), 'page_obj' : paginator.page(request.GET['page'])})
+    try:
+        page_num = request.GET['page']
+        quotes = paginator.page(page_num)
+    except MultiValueDictKeyError:
+        #if there's no page param, return the first page
+        quotes = paginator.page(1)
+
+    return render(request, "app/all_quotes.html", {'quotes' : quotes})
