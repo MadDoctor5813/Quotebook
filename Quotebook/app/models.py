24a9fec4c861a73d63f3c2c36705f8a9dd4883de
Kevin <dragn194@gmail.com>
2017-03-22 17:23:24 -0400

    Change num_ratings to an IntegerField

--- Quotebook/app/models.py
+++ Quotebook/app/models.py
@@ -11,7 +11,7 @@ class Quote(models.Model):
     attribution = models.CharField(max_length=64)
 
     rating = models.FloatField(default=0)
-    num_ratings = models.FloatField(default=0);
+    num_ratings = models.IntegerField(default=0);
 
     def __str__(self):
         return 'Quote ' + str(self.pk) + ' by ' + self.attribution
