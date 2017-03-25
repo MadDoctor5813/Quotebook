8afa348fc26255670c1bc17368dff3f700321f97
Kevin <dragn194@gmail.com>
2017-03-25 18:46:49 -0400

    Add ajax code to rate a quote

--- Quotebook/app/static/app/scripts/quote.js
+++ Quotebook/app/static/app/scripts/quote.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
-    $("#rate-btn").click(function () {
-        rating_input = $("#modal-rating-input")
+    $(".rate-btn").click(function (event) {
+        //this stops the event triggering for every button
+        event.stopImmediatePropagation()
+        quote_id = $(this).data("quote-id")
+        //find the rating input with the id that matches this button
+        rating_input = $(".rating[data-quote-id='" + quote_id + "']")
         rating = rating_input.val()
-        quote_id = rating_input.data("quote-id")
         $.ajax({
             method: "POST",
             url: "/api/rate_quote/",
             data: {
                 "rating": rating,
-                "id": quote_id
+                "id": quote_id,
+                "csrfmiddlewaretoken" : CSRF_TOKEN
             }
         })
+        //close the modal ourselves, because we stopped event propagation earlier
+        $(this).parents(".modal").modal("hide")
     })
 })
